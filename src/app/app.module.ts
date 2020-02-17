b8ba7c19572af627a1653c1f09cbff32fe33d7a2
chelsealjan <chelsealjan@gmail.com>
2020-02-17 11:20:08 -0800

    Updated global app component and added global styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { BannerComponent } from './banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
